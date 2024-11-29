efffbe234f6307d5c1f1839aa16578038bc3ab69
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-29 07:10:46 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -8,8 +8,14 @@ import pyogrio
 
 
 #Mapa de la alcaldía cuauhtemoc
+#Cargar colonias
+url="https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
+cuauhtemoc=gpd.read_file(url,driver='pyogrio')
 
 
 m = leafmap.Map(minimap_control=True,location=[19.4326, -99.1332], zoom_start=12)
-m.add_basemap("OpenTopoMap")
+#Add geojson
+leafmap.add_geojson(cuauhtemoc, m, layer_name="Cuauhtemoc",fill_color="blue",fill_opacity=0.5)
 m.to_streamlit(height=500)
+
+
