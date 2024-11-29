e3ba0ba47cf0cec2f55001556cd4ef9a2ef74c81
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-29 07:30:44 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -1,21 +1,27 @@
 import streamlit as st
-import pandas as pd
-import leafmap.foliumap as leafmap
 import geopandas as gpd
-import pyogrio
+import leafmap.foliumap as leafmap
 
+# Load GeoJSON
+url = "https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
+cuauhtemoc = gpd.read_file(url)
 
+# Sidebar for filtering
+st.sidebar.header("Filtrar por colonia")
+colonia_list = ["Todas"] + sorted(cuauhtemoc["nom_colonia"].unique())
+selected_colonia = st.sidebar.selectbox("Selecciona una colonia:", colonia_list)
 
+# Initialize the map
+m = leafmap.Map(minimap_control=True, location=[19.4326, -99.1332], zoom_start=12)
 
-#Mapa de la alcaldía cuauhtemoc
-#Cargar colonias
-url="https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
-cuauhtemoc=gpd.read_file(url,driver='pyogrio')
+# Filter and display the selected colonia
+if selected_colonia == "Todas":
+    # Add all polygons
+    m.add_geojson(cuauhtemoc, layer_name="Colonias")
+else:
+    # Filter the GeoDataFrame for the selected colonia
+    filtered_colonia = cuauhtemoc[cuauhtemoc["nom_colonia"] == selected_colonia]
+    m.add_geojson(filtered_colonia, layer_name=selected_colonia)
 
-
-m = leafmap.Map(minimap_control=True,location=[19.4326, -99.1332], zoom_start=12)
-#Add geojson
-leafmap.add_geojson(cuauhtemoc, m, layer_name="Cuauhtemoc",fill_color="blue",fill_opacity=0.5)
+# Display the map in Streamlit
 m.to_streamlit(height=500)
-
-
