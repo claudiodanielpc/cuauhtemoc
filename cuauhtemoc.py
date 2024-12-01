721de5a0ef2dab9b0dfdf8249d0d38172b7a7c5d
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-30 21:42:01 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -8,7 +8,7 @@ cuauhtemoc=read_dataframe('https://raw.githubusercontent.com/claudiodanielpc/cua
 
 st.sidebar.title('About')
 st.sidebar.info('Explore the Highway Statistics')
-
+colonia = st.sidebar.selectbox('Selecciona una colonia', cuauhtemoc['nom_colonia'].unique())
 #Mapa centrado en la alcaldía Cuauhtémoc
 
 m = leafmap.Map(center=[19.4326, -99.1332], zoom=12)
@@ -20,7 +20,15 @@ m.add_gdf(
     style={'color': '#7fcdbb', 'fillOpacity': 0.3, 'weight': 0.5},
     )
 
+selected_gdf = cuauhtemoc[cuauhtemoc['nom_colonia'] == colonia]
 
+m.add_gdf(
+    gdf=selected_gdf,
+    layer_name='selected',
+    zoom_to_layer=True,
+    info_mode=None,
+    style={'color': 'yellow', 'fill': None, 'weight': 2}
+ )
 
 #m.add_tile_layer(
 #    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
