a02e47efae29439172511c51de1466aa0168fca8
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-30 22:34:07 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -39,7 +39,7 @@ else:
         layer_name=colonia,
         zoom_to_layer=True,
         info_mode=None,
-        style={'color': 'yellow', 'fill': None, 'weight': 2}
+        style={'color': 'yellow', 'fill': None, 'weight': 4}
     )
 
 # Handle uploaded CSV
