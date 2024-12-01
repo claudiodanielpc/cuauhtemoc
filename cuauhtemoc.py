fe230d508ae038cc4b8316c2c40e3906f380aeac
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-30 21:23:21 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -1,22 +1,23 @@
 import streamlit as st
 import geopandas as gpd
-import leafmap
+import leafmap.foliumap as leafmap
 import pyogrio
 
 # Load GeoJSON
 #url = "https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
 #uauhtemoc = gpd.read_file(url, driver="pyogrio")
 
-
+st.sidebar.title('About')
+st.sidebar.info('Explore the Highway Statistics')
 
 #Mapa centrado en la alcaldía Cuauhtémoc
 
-m = leafmap.Map()
-m.add_tile_layer(
-    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-    name="Google Satellite",
-    attribution="Google",
-)
+m = leafmap.Map(center=[19.4326, -99.1332], zoom=12)
+#m.add_tile_layer(
+#    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+#    name="Google Satellite",
+#    attribution="Google",
+#)
 
 m.to_streamlit()
 
