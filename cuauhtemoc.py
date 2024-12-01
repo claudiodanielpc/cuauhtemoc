c4922d9001d003ad452c636cbad2a67ea26928ef
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-30 21:05:15 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -1,36 +1,21 @@
 import streamlit as st
 import geopandas as gpd
-import leafmap.foliumap as leafmap
+import leafmap
 import pyogrio
 
 # Load GeoJSON
 url = "https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
 cuauhtemoc = gpd.read_file(url, driver="pyogrio")
 
-# Initialize session state for the map
-if "map_initialized" not in st.session_state:
-    # Create the base map only once
-    st.session_state["map_initialized"] = True
-    st.session_state["map"] = leafmap.Map(minimap_control=True, location=[19.4326, -99.1332], zoom_start=12)
-    # Add static base layers
-    st.session_state["map"].add_basemap("CartoDB.Positron")
 
-# Sidebar for filtering
-st.sidebar.header("Filtrar por colonia")
-colonia_list = ["Todas"] + sorted(cuauhtemoc["nom_colonia"].unique())
-selected_colonia = st.sidebar.selectbox("Selecciona una colonia:", colonia_list)
 
-# Retrieve the base map from session state
-m = st.session_state["map"]
+#Mapa centrado en la alcaldía Cuauhtémoc
 
-# Dynamically update the layer
-if selected_colonia == "Todas":
-    # Add all polygons
-    m.add_geojson(cuauhtemoc, layer_name="Colonias")
-else:
-    # Filter the GeoDataFrame for the selected colonia
-    filtered_colonia = cuauhtemoc[cuauhtemoc["nom_colonia"] == selected_colonia]
-    m.add_geojson(filtered_colonia, layer_name=selected_colonia)
+m = leafmap.Map(center=[19.4326, -99.1332], zoom=12)
+m.add_tile_layer(
+    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+    name="Google Satellite",
+    attribution="Google",
+)
+m
 
-# Display the map in Streamlit
-m.to_streamlit(height=500)
