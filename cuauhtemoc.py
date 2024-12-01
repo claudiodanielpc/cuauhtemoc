ce8035552212fb7d4b891778706a1ecaefb20f9d
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2024-11-30 21:25:45 -0600

    update mapa

--- cuauhtemoc.py
+++ cuauhtemoc.py
@@ -4,8 +4,8 @@ import leafmap.foliumap as leafmap
 import pyogrio
 
 # Load GeoJSON
-#url = "https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson"
-#uauhtemoc = gpd.read_file(url, driver="pyogrio")
+
+cuauhtemoc = gpd.read_file("https://raw.githubusercontent.com/claudiodanielpc/cuauhtemoc/refs/heads/main/cuauhtemoc.geojson", driver="pyogrio")
 
 st.sidebar.title('About')
 st.sidebar.info('Explore the Highway Statistics')
@@ -13,6 +13,16 @@ st.sidebar.info('Explore the Highway Statistics')
 #Mapa centrado en la alcaldía Cuauhtémoc
 
 m = leafmap.Map(center=[19.4326, -99.1332], zoom=12)
+m.add_gdf(
+    gdf=cuauhtemoc,
+    zoom_to_layer=False,
+    layer_name='colonias',
+    info_mode='on_click',
+    style={'color': '#7fcdbb', 'fillOpacity': 0.3, 'weight': 0.5},
+    )
+
+
+
 #m.add_tile_layer(
 #    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
 #    name="Google Satellite",
